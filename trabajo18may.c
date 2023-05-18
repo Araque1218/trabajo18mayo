d1746158ad71cd2b5f02ca814ae749138819c02e
Mateo_Araque34 <131192048+Araque1218@users.noreply.github.com>
2023-05-18 12:06:11 -0500

    Creación de la matriz

---
+++ trabajo18may.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int dimension;
+
+    printf("Ingrese la dimensión de la matriz: ");
+    scanf("%d", &dimension);
+
+    // Crear la matriz
+    int matriz[dimension][dimension];
+
+    // Inicializar la matriz en 0
+    for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+            matriz[i][j] = 0;
+        }
+    }
